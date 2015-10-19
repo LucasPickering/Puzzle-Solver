e1673f9a49b148c2ed8bc620110d643b982ca8ad
Logan Moore <lmooore@gmail.com>
2015-10-19 13:27:15 -0400

    Various changes
    Added implementation notes, sorted input points, changed
    getCornerDistance() to a double and completed it.

--- src/puzzlesolver/side/SimpleSide.java
+++ src/puzzlesolver/side/SimpleSide.java
@@ -1,29 +1,38 @@
 package puzzlesolver.side;
 
+import java.util.Arrays;
+
 import puzzlesolver.Point;
 
 public final class SimpleSide implements Side {
 
+  /**
+   * List of points on the side.
+   *
+   * points[i].x = distance from left side
+   * points[i].y = height from side base
+   */
   private final Point[] points;
   private final SideType sideType;
 
   public SimpleSide(Point... points){
     this.points = points.clone();
+    Arrays.sort(this.points, 0, this.points.length, Point.PointComparator.class);
     sideType = findSideType();
   }
 
-  private Side.SideType findSideType(){
-    return null; // TODO
+  private SideType findSideType(){
+    return sideType;
   }
 
   @Override
   public int compareTo(Side side) {
-    return 0; // TODO
+    return
   }
 
   @Override
-  public float getCornerDistance() {
-    return 0; // TODO
+  public double getCornerDistance() {
+    return points[points.length - 1].x;
   }
 
   @Override
