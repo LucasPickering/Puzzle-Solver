0f04342df233ba1cdf4ae6492ef07126163d6055
Lucas Pickering <pickering.l@husky.neu.edu>
2015-10-19 08:56:53 -0400

    Added some implementation to Piece

--- src/puzzlesolver/Piece.java
+++ src/puzzlesolver/Piece.java
@@ -1,12 +1,20 @@
 package puzzlesolver;
 
+import puzzlesolver.side.Side;
+
 /**
- * An interface to represent puzzle pieces.
+ * A class to represent a 4-sided puzzle piece.
  */
 public class Piece {
 
   enum Direction {
     NORTH, EAST, SOUTH, WEST
   }
+
+  /**
+   * All sides must be non-null. Sides are ordered in the same way as {@link Direction}: NORTH,
+   * EAST, SOUTH, WEST.
+   */
+  private final Side[] sides = new Side[4];
 
 }
