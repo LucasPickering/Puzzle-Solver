9412d1aec135f79f5c7f6ccefe3b766e102244d9
Lucas Pickering <pickering.l@husky.neu.edu>
2015-10-22 16:56:48 -0400

    Added Javadoc and a final

--- src/puzzlesolver/solver/SimpleSolver.java
+++ src/puzzlesolver/solver/SimpleSolver.java
@@ -2,17 +2,25 @@ package puzzlesolver.solver;
 
 import puzzlesolver.Piece;
 
-public class SimpleSolver implements Solver {
+public final class SimpleSolver implements Solver {
 
   @Override
   public Piece[][] solve(Piece[] pieces) {
     final int width = getWidth(0, pieces.length);
-    final int length = getLength(width, pieces.length);
-    Piece[][] solution = new Piece[width][length];
+    final int height = getHeight(width, pieces.length);
+    Piece[][] solution = new Piece[width][height];
 
     return solution;
   }
 
+  /**
+   * Gets the width of a puzzle with the given perimeter and area.
+   *
+   * @param perimeter the amount of edge/corner pieces in the puzzle (positive)
+   * @param area      the amount of total pieces in the puzzle (positive)
+   * @return the width of the puzzle, in pieces
+   * @throws IllegalArgumentException if no width can be found for the given perimeter and area
+   */
   private int getWidth(int perimeter, int area) {
     final int helper = (perimeter + 4) / 2;
     final double width = (helper + Math.sqrt(helper * helper - 4 * area)) / 2;
@@ -25,7 +33,16 @@ public class SimpleSolver implements Solver {
     return roundedWidth;
   }
 
-  private int getLength(int width, int area) {
+  /**
+   * Gets the height of a puzzle with the given width and area.
+   *
+   * @param width the width of the puzzle, in pieces (positive)
+   * @param area  the amount of total pieces in the puzzle (positive)
+   * @return the height of the puzzle, in pieces
+   * @throws IllegalArgumentException if no puzzle with the given area and width exists, i.e. {@code
+   *                                  area} is not divisible by {@code width}
+   */
+  private int getHeight(int width, int area) {
     if (area % width != 0) {
       throw new IllegalArgumentException(String.format("Area (%d) is not divisible by width (%d)",
           area, width));
