d73389bb91e25cadb1e4044fe668d2e370e53213
Lucas Pickering <pickering.l@husky.neu.edu>
2015-10-26 23:56:56 -0400

    Moved getWidth and getHeight into Solver

--- src/puzzlesolver/solver/SimpleSolver.java
+++ src/puzzlesolver/solver/SimpleSolver.java
@@ -41,40 +41,5 @@ public final class SimpleSolver implements Solver {
     }
   }
 
-  /**
-   * Gets the width of a puzzle with the given perimeter and area.
-   *
-   * @param perimeter the amount of edge/corner pieces in the puzzle (positive)
-   * @param area      the amount of total pieces in the puzzle (positive)
-   * @return the width of the puzzle, in pieces
-   * @throws IllegalArgumentException if no width can be found for the given perimeter and area
-   */
-  private int getWidth(int perimeter, int area) {
-    final int helper = (perimeter + 4) / 2;
-    final double width = (helper + Math.sqrt(helper * helper - 4 * area)) / 2;
-    final int roundedWidth = (int) (width + 0.5D);
-    final double error = Math.abs(width - roundedWidth);
-    if (error > 0.1D) {
-      throw new IllegalArgumentException(String.format("Error is %f for perimeter %d and area %d",
-          error, perimeter, area));
-    }
-    return roundedWidth;
-  }
 
-  /**
-   * Gets the height of a puzzle with the given width and area.
-   *
-   * @param width the width of the puzzle, in pieces (positive)
-   * @param area  the amount of total pieces in the puzzle (positive)
-   * @return the height of the puzzle, in pieces
-   * @throws IllegalArgumentException if no puzzle with the given area and width exists, i.e. {@code
-   *                                  area} is not divisible by {@code width}
-   */
-  private int getHeight(int width, int area) {
-    if (area % width != 0) {
-      throw new IllegalArgumentException(String.format("Area (%d) is not divisible by width (%d)",
-          area, width));
-    }
-    return area / width;
-  }
 }
