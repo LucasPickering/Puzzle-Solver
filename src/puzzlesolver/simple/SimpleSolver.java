2f7528a774ca2c7ac3e487e63716841763461905
Lucas Pickering <pickering.l@husky.neu.edu>
2015-10-31 16:59:16 -0400

    Incomplete solver stuff

--- src/puzzlesolver/simple/SimpleSolver.java
+++ src/puzzlesolver/simple/SimpleSolver.java
@@ -1,10 +1,15 @@
 package puzzlesolver.simple;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import puzzlesolver.Piece;
 import puzzlesolver.PieceList;
 import puzzlesolver.enums.Direction;
 import puzzlesolver.enums.PieceType;
 import puzzlesolver.Solver;
+import puzzlesolver.enums.SideType;
 
 public final class SimpleSolver implements Solver {
 
@@ -29,6 +34,7 @@ public final class SimpleSolver implements Solver {
 
     placeCorners(pieceList);
     placeEdges(pieceList);
+    placeInternals(pieceList);
 
     return solution;
   }
@@ -58,6 +64,24 @@ public final class SimpleSolver implements Solver {
             .search(Direction.NORTH, solution[x][y - 1].getSide(Direction.SOUTH), PieceType.EDGE);
         solution[x][y] = piece;
         pieces.remove(piece);
+      }
+    }
+  }
+
+  private void placeInternals(PieceList pieces) {
+    for (int x = 1; x < width - 1; x++) {
+      for (int y = 1; y < height - 1; y++) {
+        final List<PieceType> pieceTypes = new ArrayList<>(6); // 6 possible types (efficiency!)
+        for (int i = 2; i < PieceType.values().length; i++) {
+          pieceTypes.add(PieceType.values()[i]);
+        }
+
+        final SideType[] adjacentTypes = new SideType[4];
+        for (Direction dir : Direction.values()) {
+          adjacentTypes[dir.ordinal()] = solution[x + dir.x][y + dir.y].getSide(dir).getSideType();
+        }
+
+
       }
     }
   }
