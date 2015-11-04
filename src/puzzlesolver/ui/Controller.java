126e6950142b94eaab20e1e164a2d831caa31584
Logan Moore <lmooore@gmail.com>
2015-11-04 13:49:26 -0500

    javafx hello world

--- src/puzzlesolver/ui/Controller.java
+++ src/puzzlesolver/ui/Controller.java
@@ -1,4 +1,13 @@
 package puzzlesolver.ui;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+  public Label helloWorld;
+
+  public void sayHelloWorld(ActionEvent actionEvent) {
+    helloWorld.setText("Hello, world!");
+  }
 }
