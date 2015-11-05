867c4e9ace5083320adbbe80ccad6403a032e665
Logan Moore <lmooore@gmail.com>
2015-11-05 10:34:15 -0500

    JavaDoc'd up the Constants

--- src/puzzlesolver/Constants.java
+++ src/puzzlesolver/Constants.java
@@ -1,8 +1,22 @@
 package puzzlesolver;
 
+import puzzlesolver.ui.views.TextView;
+
 public class Constants {
 
-
+  /**
+   * Number of sides to a {@link Piece}.
+   */
   public static int NUM_SIDES = 4;
+
+  /**
+   * Length of {@link Piece} sides.
+   */
   public static double SIDE_LENGTH = 10d;
+
+  // Text view
+  /**
+   * Width of {@link Piece} in {@link TextView}
+   */
+  public static int PIECE_SIZE_CHARS = 3;
 }
