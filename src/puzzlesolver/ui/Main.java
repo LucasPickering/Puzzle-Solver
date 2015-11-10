b2c5f3528034508bfbe78de4b3cfa2df845ba5aa
Logan Moore <lmooore@gmail.com>
2015-11-10 15:06:50 -0500

    Apparently I removed an empty line's indentation

--- src/puzzlesolver/ui/Main.java
+++ src/puzzlesolver/ui/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 
   public static void main(String[] args) {
     boolean console = true;
-    
+
     if (Arrays.asList(args).contains("-c") || console)
     {
       ConsoleController consoleController = new ConsoleController();
