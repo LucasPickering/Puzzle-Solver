d4e6188c44ed91764c5839b7bc19b85c7591b555
Logan Moore <lmooore@gmail.com>
2015-11-19 18:34:33 -0500

    unbroke java

--- src/puzzlesolver/Main.java
+++ src/puzzlesolver/Main.java
@@ -3,6 +3,7 @@ package puzzlesolver;
 import java.util.Arrays;
 import java.util.List;
 
+import puzzlesolver.constants.Constants;
 import puzzlesolver.ui.console.ConsoleController;
 import puzzlesolver.ui.fx.MainController;
 
@@ -10,15 +11,21 @@ public class Main {
 
   public static void main(String[] args) {
     List<String> argList = Arrays.asList(args);
-    /*Constants.VERBOSE_LEVEL = argList.stream().reduce(0, (Integer integer, String s)
-                                                     -> (s.matches("-(v)+"))
-                                                        ? integer + s.length() - 1
-                                                        : integer,
-                                                 (u, u2) -> (u + u2));*/
 
-    if (argList.contains("-c")) {
+    // this throws a compiler error if not explicitly cast
+    // see: http://stackoverflow.com/q/32891632
+    //noinspection RedundantCast
+    Constants.VERBOSE_LEVEL =
+        (int) argList.stream().reduce(0, (integer, s) -> (s.matches("-(v)+"))
+                                                         ? integer + s.length() - 1
+                                                         : integer,
+                                      (i1, i2) -> i1 + i2);
+
+    if (argList.contains("-c"))
+    {
       ConsoleController.main(args);
-    } else {
+    } else
+    {
       MainController.main(args);
     }
   }
