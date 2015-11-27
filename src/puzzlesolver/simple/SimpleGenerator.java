ef02a291a7e73a9becdef10c9a1c7ceeeae9599b
Lucas Pickering <pickering.l@husky.neu.edu>
2015-11-26 19:05:26 -0500

    Fixed x deviation

--- src/puzzlesolver/simple/SimpleGenerator.java
+++ src/puzzlesolver/simple/SimpleGenerator.java
@@ -16,8 +16,8 @@ import puzzlesolver.enums.Direction;
  */
 public class SimpleGenerator implements Generator {
 
-  private static final double MIN_X_DEVIATION = Constants.SIDE_LENGTH * 0.1d;
-  private static final double MAX_X_DEVIATION = Constants.SIDE_LENGTH * 0.3d;
+  private static final double MIN_X_DEVIATION = Constants.SIDE_LENGTH * 0.3d;
+  private static final double MAX_X_DEVIATION = Constants.SIDE_LENGTH * 0.7d;
   private static final double MIN_Y_DEVIATION = Constants.SIDE_LENGTH * 0.01d;
   private static final double MAX_Y_DEVIATION = Constants.SIDE_LENGTH * 0.1d;
 
