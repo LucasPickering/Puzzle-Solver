c3c9859777486726e679389c51ed85aa20e0de63
Lucas Pickering <pickering.l@husky.neu.edu>
2015-12-02 14:18:48 -0500

    Moved an if

--- src/puzzlesolver/polypoint/PolypointGenerator.java
+++ src/puzzlesolver/polypoint/PolypointGenerator.java
@@ -1,6 +1,6 @@
 package puzzlesolver.polypoint;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import puzzlesolver.Funcs;
@@ -19,22 +19,19 @@ public class PolypointGenerator extends RotationGenerator {
 
   @Override
   public Side generateSide(boolean flat) {
-    if (flat) {
-      return super.generateSide(true);
-    }
-
-    final List<Point> points = new ArrayList<>();
+    final List<Point> points = new LinkedList<>();
     points.add(new Point(0d, 0d)); // Add first corner
 
-    // Add midpoints
-    for (double x = Funcs.randomInRange(random, MIN_X_DIFF, MAX_X_DIFF);
-         x < Constants.SIDE_LENGTH;
-         x += Funcs.randomInRange(random, MIN_X_DIFF, MAX_X_DIFF)) {
-      points.add(new Point(x, Funcs.randomInRangeNegate(random, MIN_Y_DEVIATION, MAX_Y_DEVIATION)));
+    if (!flat) {
+      // Add midpoints
+      for (double x = Funcs.randomInRange(random, MIN_X_DIFF, MAX_X_DIFF);
+           x < Constants.SIDE_LENGTH;
+           x += Funcs.randomInRange(random, MIN_X_DIFF, MAX_X_DIFF)) {
+        points.add(new Point(x, Funcs.randomInRangeNegate(random, MIN_Y_DEVIATION, MAX_Y_DEVIATION)));
+      }
     }
 
     points.add(new Point(Constants.SIDE_LENGTH, 0d)); // Add second corner
-
     return new SimpleSide(points);
   }
 }
