b391cb8bc8ded5efb76afd435db99731be0eca25
Lucas Pickering <pickering.l@husky.neu.edu>
2016-09-11 21:12:01 -0400

    Shifting gears on how benchmarks are being stored/compared

--- benchmarks/save_results.py
+++ benchmarks/save_results.py
@@ -1,57 +1,39 @@
 import re
-import json
-import sys
-import subprocess
-from http import client
 
-ES_HOST = 'localhost:9200'
-DOC_LOCATION = '/speed/results/{}'
 RESULT_RGX = re.compile(
 	r'(\w+)\.(\w+)\s+(\d+\.\d+)d\s+(\d+)\s+(\w+)\s+(\d+)\s+(\d+\.\d+)[^0-9]+(\d+\.\d+)\s+(.+)')
-FILE_NAME_RGX = re.compile(r'(\w+)_([\d-]+_[\d.]+)')
 
-def save_results(results_file):
-	file_match = FILE_NAME_RGX.match(results_file)
-	if not file_match:
-		print("Bad file name format: " + results_file)
-		sys.exit(1)
+TEST = {
+	'Greedy': {
+		(0.2, 50): 5.0,
+		(0.2, 100): 7.5,
+		(0.1, 50): 10.0
+	},
+	'PieceType': {
+		(0.2, 50): 5.5,
+		(0.2, 100): 7.8,
+		(0.1, 50): 10.5
+	},
+	'Rotation': {
+		(0.2, 50): 6.0,
+		(0.2, 100): 8.5,
+		(0.1, 50): 11.0
+	}
+}
 
-	results = {}
-	results['date'] = file_match.group(2)
-	results['branch'] = git_branch()
-	results['commit'] = git_commit()
-	for line in open(results_file).read().splitlines():
-		match = RESULT_RGX.match(line)
-		if match:
-			groups = match.groups()
+def compare(result_dict):
+	diff = {}
+	# Populate diff with results for each benchmark
+	for result_name in result_dict:
+		results = result_dict[result_name]
+		for test_params in results:
+			if not test_params in diff:
+				diff[test_params] = []
+			diff[test_params].append((result_name, results[test_params]))
 
-			class_results = {}
-			results[groups[0]] = class_results
+	# Sort the list by time
+	for test_params in diff:
+		diff[test_params] = sorted(diff[test_params], key=lambda value: value[1])
+	return diff
 
-			method_results = {}
-			class_results[groups[1]] = method_results
-
-			parameters = {}
-			method_results['parameters'] = parameters
-			parameters['simplicity'] = float(groups[2])
-			parameters['size'] = int(groups[3])
-
-			method_results['iterations'] = int(groups[5])
-			method_results['score'] = float(groups[6])
-			method_results['error'] = float(groups[7]) / method_results['score'] * 100
-	return results
-
-def git_commit():
-	return subprocess.check_output('git rev-parse HEAD'.split(' ').rstrip())
-
-def git_branch():
-	return subprocess.check_output('git rev-parse --abbrev-ref HEAD'.split(' ')).rstrip()
-
-def send_to_es(results):
-	connection = client.HTTPConnection(ES_HOST)
-	connection.request('PUT', DOC_LOCATION.format(results['date']), json.dumps(results))
-	print(connection.getresponse().read())
-	connection.close()
-
-results = save_results('results/2016-09-10_15.43.19.txt')
-send_to_es(results)
+print(compare(TEST))
